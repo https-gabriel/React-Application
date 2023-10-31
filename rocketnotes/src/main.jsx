de3e075c4794b4fd0cdf7b3fe6ba4a4c16e51dfd
Gabriel Farias <farias2752@gmail.com>
2023-10-31 07:35:08 -0300

    ThemeProvider 'theme'

--- rocketnotes/src/main.jsx
+++ rocketnotes/src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Details } from './pages/Details'
+import { ThemeProvider } from 'styled-components';
+import { Theme } from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details/>
+    <ThemeProvider theme={Theme}>
+      <Details/>
+    </ThemeProvider>
   </React.StrictMode>,
 )
