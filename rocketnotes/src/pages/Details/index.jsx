289c05f8dc7c455ae1f1d5d8317ed65bd9d9a562
Gabriel Farias <farias2752@gmail.com>
2023-10-31 07:25:40 -0300

    CSS-in-Js

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,10 +1,10 @@
-import "./styles.css";
+import {Container } from "./styles.js";
 
 export function Details() {
   return (
-    <>
+    <Container>
       <h1>Hello Word!</h1>
       <span>Gabriel Farias</span>
-    </>
+    </Container>
   )
 };
