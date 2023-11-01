d8254e6f0c2833745a5a480da249fc1742380bae
Gabriel <farias2752@gmail.com>
2023-10-31 21:23:46 -0300

    Componente

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
 import {Container } from "./styles.js";
 
+import { Button } from "../../components/Button"
+
 export function Details() {
   return (
     <Container>
       <h1>Hello Word!</h1>
       <span>Gabriel Farias</span>
+
+      <Button/>
     </Container>
   )
 };
