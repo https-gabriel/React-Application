40a8e039bbd801ec251394aaa4f3acb0d9752910
Gabriel Farias <farias2752@gmail.com>
2023-11-02 21:31:39 -0300

    Criação dos components: Tag, Section, ButtonText, Button. Ultilização do children e melhores práticas

--- rocketnotes/src/components/Header/index.jsx
+++ rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,23 @@
-import { Container } from './styled'
+import {RiShutDownLine} from 'react-icons/ri';
+import { Container, Profile, Logout } from './styles'
 
 export function Header() {
     return(
         <Container>
-            
+            <Profile>
+                <img src="https://github.com/SoftEngineerGabriel.png" 
+                 alt="Foto user"
+                />
+
+                <div>
+                    <span>Bem vindo,</span>
+                    <strong>Gabriel Farias</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
         </Container>
     )
 }
--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,14 +1,42 @@
-import {Container } from "./styles.js";
+import { Container, Links, Content } from "./styles.js";
 
+import { Tag } from "../../components/Tag";
 import { Header } from "../../components/Header";
 import { Button } from '../../components/Button';
+import { Section } from "../../components/Section";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
   return (
     <Container>
       <Header/>
-      
-      <Button title="Voltar"/>
+
+      <main>
+        <Content>
+        <ButtonText title={"Excluir nota"}/>
+        
+        <h1>
+          Introducao ao React
+        </h1>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, numquam est sunt corrupti aut ex quas dolore sint quidem recusandae doloribus ipsam quaerat quibusdam molestias. Distinctio, natus corrupti. Magnam, dolorem.
+        </p>
+        <Section title={"Links uteis"}>
+          <Links>
+            <li><a href="#"> https://github.com/SoftEngineerGabriel</a></li>
+            <li><a href="#"> https://github.com/SoftEngineerGabriel</a></li>
+          </Links>
+        </Section>
+
+        <Section title={"Marcadores"}>
+          <Tag title={"Express"}/>
+          <Tag title={"Node"}/>
+        </Section>
+
+        <Button title="Voltar"/>
+      </Content>
+      </main>
     </Container>
   )
 };
