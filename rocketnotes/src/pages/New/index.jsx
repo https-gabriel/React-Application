cb7934ead5a9e043e0d6b772b53f2d9f18b0a5aa
Gabriel Farias <farias2752@gmail.com>
2023-11-15 18:21:21 -0300

    Finalizando o New

--- rocketnotes/src/pages/New/index.jsx
+++ rocketnotes/src/pages/New/index.jsx
@@ -3,6 +3,7 @@ import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
 import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
 
 import { Container, Form } from './styles';
 
@@ -26,6 +27,14 @@ export function New() {
                         <NoteItem isNew placeholder="Novo link"/>
                     </Section>
 
+                    <Section title="Marcadores">
+                        <div className='tags'>
+                            <NoteItem value="React"/>
+                            <NoteItem isNew placeholder="Novo tag"/>
+                        </div>
+                    </Section>
+
+                    <Button title="Salvar"/>
                 </Form>
 
                 
